e2ccfeac8c963e2042786335b908b38b674e43ff
trabajomilenad <trabajomilenad@gmail.com>
2026-06-10 18:05:12 -0300

    cambio 1 de Milena

---
+++ app.js
@@ -0,0 +1,14 @@
+
+alert("¡Hola! Bienvenidos a Heladería Artesanal.");
+
+console.log("Bienvenido a la página de Heladería Artesanal.");
+
+let tituloHelados = "Helados Artesanales";
+let descripcionHelados = "Disfrutá de una gran variedad de sabores elaborados con ingredientes de primera calidad.";
+let promocionHelados = "2x1 en todos los sabores los lunes y miércoles.";
+
+document.getElementById("tituloCurso").innerHTML = tituloHelados;
+document.getElementById("descripcionCurso").innerHTML = descripcionHelados;
+document.getElementById("duracionCurso").innerHTML = promocionHelados;
+;
+
